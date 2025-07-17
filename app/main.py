c91be5077414b192bec8a131abeda58604f67382
Garavirod <rodrigogarciaavila26@gmail.com>
2025-07-16 18:15:33 -0600

    feat(k8s): Implement readiness and liveness checks with startup delay for testing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from api import books, genres, authors
 from fastapi.responses import HTMLResponse
 from pathlib import Path
+from environment import ( FAIL_LIVENESS, FAIL_READINESS, DELAY_STARTUP)
 import socket
+import random
+import time
+
+# For testing purposes, K8s Probes
+delay_startup = DELAY_STARTUP == 'true'
+fail_liveness = FAIL_LIVENESS == 'true'
+fail_readiness = random.random() < 0.5 if FAIL_READINESS == False else False
+
+print(f"Delay startup: {delay_startup}")
+print(f"Fail liveness: {fail_liveness}")    
+print(f"Fail readiness: {fail_readiness}")
+
+
 app = FastAPI()
 
 # Serve static content
@@ -16,11 +30,33 @@ async def get_hostname():
     hostname = socket.gethostname()
     return {"hostname": hostname}
 
+@app.get("/ready")
+async def readiness_check():
+    if fail_readiness:
+        raise HTTPException(status_code=503, detail="Service is not ready")
+    return {"status": "ready", "message": "Service is ready"}
+
+@app.get("/health")
+async def liveness_check():
+    if fail_liveness:
+        raise HTTPException(status_code=503, detail="Service is not healthy")
+    return {"status": "healthy", "message": "Service is healthy"}
+
+
 # Serve the index.html file
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     index_file_path = Path("static/index.html")
     return index_file_path.read_text()
+
+# Delay startup endpoint
+if delay_startup:
+    start =  time.time()
+    # Simulate a delay for startup, purposefully blocking event loop
+    # to illustrate startup probes
+    print(f"Delaying startup for seconds 60s")
+    while time.time() - start < 60000:
+        pass  
 
 # Include router from endpoints
 app.include_router(books.router)
